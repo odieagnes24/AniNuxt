c7b824dc12b48871a677f51afe239f6cdaef71aa
LAPTOP-3E7J5FB1\Odie <odieagnes@gmail.com>
2024-08-26 18:28:58 +0800

    fix carousel contents responsiveness

--- components/CarouselCard.vue
+++ components/CarouselCard.vue
@@ -3,21 +3,31 @@
         {{ error.statusCode }} | {{ error.message }}
     </p>
 
-    <Carousel :wrapAround=true :autoplay="5000" v-if="status === 'success'" class="grow">
+    <Carousel :wrapAround=true :autoplay="5000" v-if="status === 'success'" class="grow ">
         <Slide v-for="anime in animes.results" :key="anime.id">
             <div 
                 class="carousel__item rounded h-96 p-10 bg-gradient-to-r from-cyan-500 to-blue-500" 
             >
-                <div class="flex self-center">
-                    <div class="flex-none w-56 self-center">
-                        <img
-                            class="w-52 mx-auto rounded shadow-sm"
-                            :src="anime.image"
-                            alt="Anime Thumb" 
-                        />
+                <div class="flex self-center justify-center">
+                    <div class="flex-none w-56 self-center relative">
+                        <NuxtLink :to="`/anime/${anime.id}/watch/${anime.episodeNumber}`">
+                            <img
+                                class="w-56 mx-auto rounded shadow-sm"
+                                :src="anime.image"
+                                alt="Anime Thumb" 
+                            />
+
+                            <div v-if="anime.episodeNumber" class="absolute top-0 right-0 rounded-tr rounded-bl p-2 bg-success block md:hidden">
+                                <p class="font-medium">EP{{ anime.episodeNumber }}</p>
+                            </div>
+
+                            <div class="absolute w-56 bottom-0 bg-purple-600 rounded block md:hidden">
+                                <p class="truncate">{{ anime.title }}</p>
+                            </div>
+                        </NuxtLink>
                     </div>
-                    <div class="flex-initial w-full text-start px-5">
-                        <p class="text-6xl">{{ anime.title }}</p>
+                    <div class="flex-initial w-full text-start px-5 hidden md:block">
+                        <p class="lg:text-6xl">{{ anime.title }}</p>
                         <p class="mt-4">Episode {{ anime.episodeNumber }}</p>
                         <div>
                             <template v-for="genre in anime.genres">
@@ -28,7 +38,6 @@
                         <NuxtLink :to="`/anime/${anime.id}/watch/${anime.episodeNumber}`">
                             <button class="btn btn-primary mt-4">Watch</button>
                         </NuxtLink>
-                        
                     </div>
                 </div>
             </div>
